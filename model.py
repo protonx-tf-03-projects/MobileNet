49be50df48750d50b81b970569dae624457ca039
NKNK-vn <khoi.nkn12@gmail.com>
2021-08-19 20:20:28 +0700

    Base model(Non-Hyperparameters)

---
+++ model.py
@@ -0,0 +1,71 @@
+from threading import main_thread
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, DepthwiseConv2D, BatchNormalization, Activation, AveragePooling2D
+
+class MobileNetV1:
+    def __init__(self, img_size, num_classes = 1000):
+        self.img_size = img_size
+        self.num_classes = num_classes
+        self.model = Sequential()
+
+    def Standard_Conv(self, filter, stride):
+        self.model.add(Conv2D(filters=filter, kernel_size=3, strides=stride, input_shape=(self.img_size, self.img_size, 3)))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation('relu'))
+        return self.model
+
+    def Depthwise_Layer(self, stride, padding = 'same'):
+        self.model.add(DepthwiseConv2D(kernel_size=3, strides=stride, padding=padding))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation('relu'))
+        return self.model
+
+    def Pointwise_Layer(self, filter, stride):
+        self.model.add(Conv2D(filters=filter, kernel_size=1, strides=stride))
+        self.model.add(BatchNormalization())
+        self.model.add(Activation('relu'))
+        return self.model
+
+    def build_model(self):
+        #Block 1
+        self.model = self.Standard_Conv(32, 2)
+        self.model = self.Depthwise_Layer(1)
+        self.model = self.Pointwise_Layer(64, 1)
+        #Block 2
+        self.model = self.Depthwise_Layer(2)
+        self.model = self.Pointwise_Layer(128, 1)
+        #Block 3
+        self.model = self.Depthwise_Layer(1)
+        self.model = self.Pointwise_Layer(128, 1)
+        #Block 4
+        self.model = self.Depthwise_Layer(2)
+        self.model = self.Pointwise_Layer(256, 1)
+        #Block 5
+        self.model = self.Depthwise_Layer(1)
+        self.model = self.Pointwise_Layer(256, 1)
+        #Block 6
+        self.model = self.Depthwise_Layer(2)
+        self.model = self.Pointwise_Layer(512, 1)
+        #Block 7 >> 11
+        for i in range(5):
+            self.model = self.Depthwise_Layer(1)
+            self.model = self.Pointwise_Layer(512, 1)
+        #Block 12
+        self.model = self.Depthwise_Layer(2)
+        self.model = self.Pointwise_Layer(1024, 1)
+        #Block 13
+        self.model = self.Depthwise_Layer(1)
+        self.model = self.Pointwise_Layer(1024, 1)
+        #Fully Connected
+        self.model.add(AveragePooling2D(pool_size = (7, 7), strides=1))
+        self.model.add(Flatten())
+        self.model.add(Dense(units=self.num_classes, activation='softmax'))
+        return self.model
+
+# if __name__ == '__main__':
+#     model = MobileNetV1(224)
+#     print(model.build_model().summary())
+        
+
