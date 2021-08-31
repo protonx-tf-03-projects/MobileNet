e47486d33b68a2d8068277596abe815f6d73e550
CodeProtonX <tranlequynh.uet@gmail.com>
2021-08-31 20:48:14 +0700

    test-model-mobilenetv1

--- predict.py
+++ predict.py
@@ -1,24 +1,51 @@
 import os
 from argparse import ArgumentParser
+import tensorflow as tf
+from model import MobileNetV1
+import numpy as np
+
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--batch-size", default=64, type=int)
-    parser.add_argument("--epochs", default=1000, type=int)
 
-    # FIXME
+    parser = ArgumentParser()
+    parser.add_argument("--test-file-path", type=str, required=True)
+    parser.add_argument("--model-path", default='./model/', type=str, required=True)
+    parser.add_argument("--image-size", default=224, type=int)
+    parser.add_argument("--image-channels", default=3, type=int)
+    parser.add_argument("--rho", help='modify img_size, should be (0:1]',default=1.0, type=float)
+    
+    home_dir = os.getcwd()
     args = parser.parse_args()
+    rho = args.rho
+    image_size = args.image_size
 
     # FIXME
     # Project Description
-
-    print('---------------------Welcome to ${name}-------------------')
-    print('Github: ${accout}')
+    print('---------------------Welcome to ProtonX MobileNet-------------------')
+    print('Github: https://github.com/protonx-tf-03-projects/MobileNet')
     print('Email: ${email}')
     print('---------------------------------------------------------------------')
-    print('Training ${name} model with hyper-params:') # FIXME
+    print('Testing MobileNet model with hyper-params:')
+    print('===========================')
+    for i, arg in enumerate(vars(args)):
+        print('{}.{}: {}'.format(i, arg, vars(args)[arg]))
     print('===========================')
 
-    # FIXME
-    # Do Training
+
+    # Loading Model
+    mobilenet = tf.keras.models.load_model(args.model_path) 
+
+    # Load test images from folder
+    image = tf.keras.preprocessing.image.load_img(args.test_file_path)
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr])  # Convert single image to a batch.
+    x = tf.image.resize(
+        input_arr, [(int(image_size*rho)), (int(image_size*rho))]
+    )
+    x = x / 255
+
+    predictions = mobilenet.predict(x)   
+    print('---------------------Prediction Result: -------------------')
+    print('Output Softmax: {}'.format(predictions))
+    print('This image belongs to class: {}'.format(np.argmax(predictions), axis=1))
 
